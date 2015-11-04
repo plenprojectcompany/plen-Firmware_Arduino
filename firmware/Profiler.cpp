52e4072a12af9d0242918d3f750985dc8b466f93
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2015-11-04 20:19:26 +0900

    Add copyright to Profiler.

--- firmware/Profiler.cpp
+++ firmware/Profiler.cpp
@@ -1,3 +1,12 @@
+/*
+	Copyright (c) 2015,
+	- Kazuyuki TAKASE - https://github.com/Guvalif
+	- PLEN Project Company Ltd. - http://plen.jp
+
+	This software is released under the MIT License.
+	(See also : http://opensource.org/licenses/mit-license.php)
+*/
+
 #include "Arduino.h"
 #include "Profiler.h"
 
